bc72e9a963e6851a77c4a970ff5b2f66e81a0bc6
Albert Khoury <albert.khoury96@gmail.com>
2018-06-08 17:26:37 -0400

    All Files now Initialized
    All files including the PDF Directory have now been implemented.

---
+++ app/src/main/java/com/example/albertkhoury/chatbotsofwerx/HelpPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.albertkhoury.chatbotsofwerx;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class HelpPageActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_page_activity);
+    }
+}
---
+++ app/src/main/java/com/example/albertkhoury/chatbotsofwerx/pdfViewerActivity.java
@@ -0,0 +1,77 @@
+package com.example.albertkhoury.chatbotsofwerx;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.shockwave.pdfium.PdfDocument;
+
+import java.util.List;
+
+public class pdfViewerActivity extends Activity implements OnPageChangeListener, OnLoadCompleteListener {
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String SAMPLE_FILE = "Ben Clark - Red Team Field Manual.pdf";
+    PDFView pdfView;
+    Integer pageNumber = 0;
+    String pdfFileName;
+    Button returnToSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pdf_viewer);
+
+        pdfView= (PDFView)findViewById(R.id.pdfView);
+        displayFromAsset(SAMPLE_FILE);
+    }
+
+    private void displayFromAsset(String assetFileName) {
+        pdfFileName = assetFileName;
+
+        pdfView.fromAsset(SAMPLE_FILE)
+                .defaultPage(pageNumber)
+                .enableSwipe(true)
+
+                .swipeHorizontal(false)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
+    }
+
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        pageNumber = page;
+        setTitle(String.format("%s %s / %s", pdfFileName, page + 1, pageCount));
+    }
+
+
+
+    public void loadComplete(int nbPages) {
+        PdfDocument.Meta meta = pdfView.getDocumentMeta();
+        printBookmarksTree(pdfView.getTableOfContents(), "-");
+
+    }
+
+    public void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
+        for (PdfDocument.Bookmark b : tree) {
+
+            Log.e(TAG, String.format("%s %s, p %d", sep, b.getTitle(), b.getPageIdx()));
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.getChildren(), sep + "-");
+            }
+        }
+    }
+
+
+}
